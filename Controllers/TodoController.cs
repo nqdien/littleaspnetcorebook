ba4c3e927c7a0bef54039e987b1b24e1731af3ca
Dien Nguyen <diennq@mti-tech.vn>
2017-10-18 17:21:14 +0700

    add service

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -1,11 +1,31 @@
+using System.Threading.Tasks;
+using AspNetCoreTodo.Models;
+using AspNetCoreTodo.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCoreTodo.Controllers
 {
     public class TodoController : Controller
     {
-        public IActionResult Index() {
-            
+        private readonly ITodoItemService _todoItemService;
+
+        public TodoController(ITodoItemService todoItemService)
+        {
+            _todoItemService = todoItemService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            // Get to-do items from database
+            // Put items into a model
+            // Pass the view to a model and render
+            // return View();
+            var todoItems = await _todoItemService.GetIncompleteItemsAsync();
+            var model = new TodoViewModel()
+            {
+                Items = todoItems
+            };
+
+            return View(model);
         }
     }
 }
